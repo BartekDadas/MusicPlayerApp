771ef3c4f2b418d493c58d4a680a032987542d78
Bartek <bartosz.dadas@wp.pl>
2021-04-20 20:17:11 +0200

    change CheckLogin for CheckUser

--- MusicPlayerLog/CheckLoginAndPassword.java
+++ MusicPlayerLog/CheckLoginAndPassword.java
@@ -5,14 +5,13 @@ import MusicPlayerRegister.User;
 
 public class CheckLoginAndPassword {
 
-    void checkLogin(String login, String password) {
+    void checkUser(String login, String password) {
         Database db = new Database();
         for(User x: db.users) {
             if (x.equals(new User(login, password))) {
 
             }
+            System.out.println("Access Denied");
         }
     }
-
-
 }
